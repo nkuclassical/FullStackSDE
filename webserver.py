6b32d4e2459ce342f35f60a08470423caf78e56a
Yanqiao <yzhan14@stevens.edu>
2016-04-06 19:03:23 -0400

    add post method

--- webserver.py
+++ webserver.py
@@ -1,32 +1,67 @@
 # coding=utf-8
-import urllib, sys
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import urllib, sys
+import cgi
 
 class WebServerHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        if self.path.endswith("/hello"):
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            message = ""
-            message += "<html><body>Hello!</body></html>"
-            self.wfile.write(message)
-            print message
-            return
+        try:
+            if self.path.endswith("/hello"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Hello!</h1>"
+                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
+                return
 
-        elif self.path.endswith(urllib.quote('你好'.decode(sys.stdin.encoding).encode('utf8'))):
-            self.send_response(200)
+
+            elif self.path.endswith(urllib.quote('你好'.decode(sys.stdin.encoding).encode('utf8'))):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>"
+                output += "<h1>你好!</h1>"
+                output += '''<form method='POST' enctype='multipart/form-data' action='/你好'><h2>你想说些什么?</h2><input name="messageCN" type="text" ><input type="submit" value="Submit"> </form>'''
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
+                return
+        except IOError:
+            self.send_error(404, 'File Not Found: %s' % self.path)
+
+    def do_POST(self):
+        try:
+            self.send_response(301)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            message = ""
-            message += "<html><body>你好!<br /><a href='/hello'>Back to hello</a></body></html>"
-            self.wfile.write(message)
-            print message
-            return
-        else:
-            self.send_error(404, 'File Not Found: %s' % self.path)
+            ctype, pdict = cgi.parse_header(
+                self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                if fields.get('message'):
+                    messagecontent = fields.get('message')
+                elif fields.get('messageCN'):
+                    messagecontent = fields.get('messageCN')
+            output = ""
+            output += "<html><body>"
+            output += " <h2> Okay, how about this: </h2>"
+            output += "<h1> %s </h1>" % messagecontent[0]
+            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+            output += "</body></html>"
+            self.wfile.write(output)
+            print output
+        except:
+            pass
+
 
 def main():
     try:
