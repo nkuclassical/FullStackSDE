38807070f0c83741b7562bcce8be188a150a6dc4
Yanqiao <yzhan14@stevens.edu>
2016-04-09 00:23:54 -0400

    add another page

--- project.py
+++ project.py
@@ -29,6 +29,22 @@ def HelloWorld():
         output += '<hr>'
     return output
 
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+        output += i.price
+        output += '</br>'
+        output += i.description
+        output += '</br>'
+        output += '</br>'
+    return output
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0',port=5000)
